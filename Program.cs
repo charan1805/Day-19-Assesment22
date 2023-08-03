cfca5b3d344bb6c51f83f73e9107754dad68418b
charan1805 <sreecharanmudhiraj@gmail.com>
2023-08-03 15:29:44 +0530

    first class

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Source source = new Source
+            {
+                Id = 1,
+                Name = "SreeCharan",
+                Age = 22,
+                Occupation = "Software Engineer"
+
+            };
+
+            Destination destination = new Destination();
+
+            PropertyMapper.MapProperties(source, destination);
+
+            Console.WriteLine("Mapped properties in the Destination class:");
+            Console.WriteLine($"Id: {destination.Id}");
+            Console.WriteLine($"Name: {destination.Name}");
+            Console.WriteLine($"Occupation: {destination.Occupation}");
+        }
+    }
+}
